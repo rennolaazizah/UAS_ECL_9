e57699572333232c7f820bf2ac87ee1dddc9139f
Rennola Azizah <47214787+rennolaazizah@users.noreply.github.com>
2019-02-06 06:49:28 +0700

    Add files via upload

--- db_laravel_siswa.sql
+++ db_laravel_siswa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 31 Jan 2019 pada 16.22
--- Versi Server: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Waktu pembuatan: 06 Feb 2019 pada 00.47
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -64,7 +64,8 @@ CREATE TABLE `t_kelas` (
 INSERT INTO `t_kelas` (`id_kelas`, `nama_kelas`, `jurusan`, `created_at`, `updated_at`) VALUES
 (1, 'XII-RPL1', 'Rekayasa Perangkat Lunak', NULL, NULL),
 (2, 'XII-RPL2', 'Rekayasa Perangkat Lunak', NULL, NULL),
-(3, 'RPL2', 'dkv', '2019-01-30 21:07:10', '2019-01-30 21:07:10');
+(3, 'XXI-RPL3', 'Rekayasa Perangkat Lunak', '2019-01-30 21:07:10', '2019-02-05 16:45:49'),
+(11, 'XXI-RPL4', 'Rekayasa Perangkat Lunak', '2019-02-05 16:45:56', '2019-02-05 16:45:56');
 
 -- --------------------------------------------------------
 
@@ -87,7 +88,7 @@ CREATE TABLE `t_login` (
 --
 
 INSERT INTO `t_login` (`id_login`, `nama_user`, `username`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin', '$2y$10$4Nq5ZYamVa21Q/ajKXhAgeHj6hVDrSL5V13EPnjE6tEfYAN8hYH9W', 'nRrSWszfQyNMNyAYQaIXcGezC93o9rBCtLnrpqoeXUs8YvRCdUQoVWX156Tq', '2019-01-31 05:30:20', '2019-01-31 05:30:20');
+(1, 'Admin', 'admin', '$2y$10$4Nq5ZYamVa21Q/ajKXhAgeHj6hVDrSL5V13EPnjE6tEfYAN8hYH9W', 'gQHNUcvrfv9RV3G3VmGzPPIhonNXeyRo58nDkWnZ2kUxSo7mpdKqVV816qHb', '2019-01-31 05:30:20', '2019-01-31 05:30:20');
 
 -- --------------------------------------------------------
 
@@ -108,51 +109,58 @@ CREATE TABLE `t_siswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data untuk tabel `t_siswa`
+--
+
+INSERT INTO `t_siswa` (`nis`, `nama_lengkap`, `jenis_kelamin`, `alamat`, `no_telp`, `id_kelas`, `created_at`, `updated_at`, `foto`) VALUES
+(10515025, 'Rennola Azizah', 'P', 'Jln. haurmekar B 12, Kota bandung. Kecamatan Colong Sadang serang', '081394968010', 1, '2019-02-05 16:46:52', '2019-02-05 16:46:52', '10515025.jpg');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `migrations`
+-- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_kelas`
+-- Indeks untuk tabel `t_kelas`
 --
 ALTER TABLE `t_kelas`
   ADD PRIMARY KEY (`id_kelas`);
 
 --
--- Indexes for table `t_login`
+-- Indeks untuk tabel `t_login`
 --
 ALTER TABLE `t_login`
   ADD PRIMARY KEY (`id_login`);
 
 --
--- Indexes for table `t_siswa`
+-- Indeks untuk tabel `t_siswa`
 --
 ALTER TABLE `t_siswa`
   ADD PRIMARY KEY (`nis`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `t_kelas`
+-- AUTO_INCREMENT untuk tabel `t_kelas`
 --
 ALTER TABLE `t_kelas`
-  MODIFY `id_kelas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_kelas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `t_login`
+-- AUTO_INCREMENT untuk tabel `t_login`
 --
 ALTER TABLE `t_login`
   MODIFY `id_login` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
